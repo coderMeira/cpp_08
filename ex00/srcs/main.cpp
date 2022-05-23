59eaa666a6f3a90e22dea6904b226f10eefb2212
Frederico Meira <fredericomeiraa@gmail.com>
2022-05-23 18:15:12 +0100

    ex00 and finishing ex01

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -2,18 +2,26 @@
 //*Template by pulgamecanica*//
 //***************************//
 
+#include <list>
+#include <vector>
+#include <iostream>
 #include "easyfind.hpp"
 
-int	main(void)
-{
-	int intArray[] = {1, 2, 3, 4, 5, 6, 7};
-	int	i = 2;
-	try
-	{
-		easyfind(intArray, i);
+int 	main() {
+	std::vector<int>	vector;
+	int					i;
+
+	i = 22;
+	vector.push_back(21);
+	vector.push_back(42);
+	vector.push_back(80);
+	vector.push_back(42);
+	try{
+		std::cout << "Return value of search 42 in vector: " << *easyfind(vector, i) << std::endl;
 	}
 	catch (const char* msg){
 		std::cout << i << msg;
 	}
+
 	return (0);
 }
---
+++ ex01/srcs/Span.cpp
@@ -0,0 +1,76 @@
+//***************************//
+//*Template by pulgamecanica*//
+//***************************//
+
+#include "Span.hpp"
+
+Span::Span(int i) : N_(i) {
+}
+
+Span::Span(const Span& param) {
+	*this = param;
+}
+
+Span::~Span() {
+	std::cout << "Span" << " destroyed" << std::endl;
+}
+
+Span& Span::operator= (const Span& param) {
+	N_ = param.N_;
+	list_ = param.list_;
+	return (*this);
+}
+
+void	Span::addNumber(int	i)
+{
+	if (list_.size() == N_)
+		throw (std::length_error("Vector is already full"));
+	list_.push_back(i);
+}
+
+int		Span::shortestSpan(void)
+{
+	if (list_.size() <= 1)
+		throw (std::logic_error("Need at least two numbers stored to find span"));
+
+	std::list<int>::iterator curr = list_.begin();
+	std::list<int>::iterator next = curr++;
+	std::list<int>::iterator end = list_.end();
+
+	int min = std::abs(static_cast<int>(*(curr++)) - static_cast<int>(*(next++)));
+	int tmp;
+	while (curr != end)
+	{
+		tmp = std::abs(static_cast<int>(*(curr++)) - static_cast<int>(*(next++)));
+		if (tmp < min)
+			min = tmp;
+	}
+	return (min);
+}
+
+int		Span::longestSpan(void)
+{
+	if (list_.size() <= 1)
+		throw (std::logic_error("Need at least two numbers stored to find span"));
+
+	std::list<int>::iterator curr = list_.begin();
+	std::list<int>::iterator next = curr++;
+	std::list<int>::iterator end = list_.end();
+
+	int min = std::abs(static_cast<int>(*(curr++)) - static_cast<int>(*(next++)));
+	int tmp;
+	while (curr != end)
+	{
+		tmp = std::abs(static_cast<int>(*(curr++)) - static_cast<int>(*(next++)));
+		if (tmp > min)
+			min = tmp;
+	}
+	return (min);
+}
+
+std::ostream& operator<<(std::ostream& s, const Span& param) {
+	// s << param.CONST_METHOD()
+	(void)param;
+	return (s);
+}
+
